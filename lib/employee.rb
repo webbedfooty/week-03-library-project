80fa13deab7368abed518cce91e69e158c3f9276
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-31 13:53:41 -0500

    updated libraries to library id in relationship in employee

--- lib/employee.rb
+++ lib/employee.rb
@@ -7,6 +7,6 @@ class Employee < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
-  #validates :email, uniqueness: true
+  validates :email, uniqueness: true
 
 end
--- lib/library.rb
+++ lib/library.rb
@@ -9,8 +9,8 @@ class Library < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
   validates :phone, presence: true
-  #validates :name, uniqueness: true
-  #validates :address, uniqueness: true
-  #validates :phone, uniqueness: true
+  validates :name, uniqueness: true
+  validates :address, uniqueness: true
+  validates :phone, uniqueness: true
 
 end
