1efe68fe909e960d8b1a5b492cf819230325340c
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-31 10:54:05 -0500

    created CreateLibariesTable

---
+++ db/migrate/001_create_library_table.rb
@@ -0,0 +1,9 @@
+class CreateLibrariesTable < ActiveRecord::Migration
+  def change
+    create_table :libraries do |t|
+      t.string :name    #must be present and unique
+      t.string :address #must be present and unique
+      t.string :phone   #must be present and unique
+    end
+  end
+end
