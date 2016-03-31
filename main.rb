7f2d9dcb0e6780f24118136d46487f560788a6b4
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-31 10:35:01 -0500

    set up Library file

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 require_relative "config/environment.rb"
+require_relative "lib/Library.rb"
+require_relative "lib/Employee.rb"
+require_relative "lib/Book.rb"
+require_relative "lib/Patron.rb"
 require "active_record"
 require "pry"
 require "yaml"
