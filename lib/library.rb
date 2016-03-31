fd4dd960e49bfab8b97eed7a6a08c705c8b83164
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-31 12:24:04 -0500

    Finished library class

--- lib/library.rb
+++ lib/library.rb
@@ -6,37 +6,8 @@
 
 class Library < ActiveRecord::Base
 
-  def test_validator_should_fail_library_with_no_name
-    i = Library.create(name: "Bennet Martin", address: "14th and O Street", phone: "312-999-6565")
-    assert(i.my_valid?, "should be valid at creation")
-
-    i.name = nil
-    refute(i.my_valid?, "should be invalid without a name")
-
-    i.name = ""
-    refute(i.my_valid?, "should be invalid with an empty name")
-  end
-
-  def test_validator_should_fail_library_with_no_address
-    i = Library.create(name: "Bennet Martin", address: "14th and O Street", phone: "312-999-6565")
-    assert(i.my_valid?, "should be valid at creation")
-
-    i.address = nil
-    refute(i.my_valid?, "should be invalid without a name")
-
-    i.address = ""
-    refute(i.my_valid?, "should be invalid with an empty name")
-  end
-
-  def test_validator_should_fail_library_with_no_phone
-    i = Library.create(name: "Bennet Martin", address: "14th and O Street", phone: "312-999-6565")
-    assert(i.my_valid?, "should be valid at creation")
-
-    i.phone = nil
-    refute(i.my_valid?, "should be invalid without a name")
-
-    i.phone = ""
-    refute(i.my_valid?, "should be invalid with an empty name")
-  end
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true 
 
 end
--- tests/library_test.rb
+++ tests/library_test.rb
@@ -26,4 +26,37 @@ class LibraryTest < Minitest::Test
     assert_equal("402-470-1009", library.phone)
   end
 
+  def test_validator_should_fail_library_with_no_name
+    i = Library.create(name: "Bennet Martin", address: "14th and O Street", phone: "312-999-6565")
+    assert(i.valid?, "should be valid at creation")
+
+    i.name = nil
+    refute(i.valid?, "should be invalid without a name")
+
+    i.name = ""
+    refute(i.valid?, "should be invalid with an empty name")
+  end
+
+  def test_validator_should_fail_library_with_no_address
+    i = Library.create(name: "Bennet Martin", address: "14th and O Street", phone: "312-999-6565")
+    assert(i.valid?, "should be valid at creation")
+
+    i.address = nil
+    refute(i.valid?, "should be invalid without a name")
+
+    i.address = ""
+    refute(i.valid?, "should be invalid with an empty name")
+  end
+
+  def test_validator_should_fail_library_with_no_phone
+    i = Library.create(name: "Bennet Martin", address: "14th and O Street", phone: "312-999-6565")
+    assert(i.valid?, "should be valid at creation")
+
+    i.phone = nil
+    refute(i.valid?, "should be invalid without a name")
+
+    i.phone = ""
+    refute(i.valid?, "should be invalid with an empty name")
+  end
+
 end
