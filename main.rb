df73879f4789f0921bcf14036ead2a5837cdad5d
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-04-01 14:38:09 -0500

    updated menu choices switched from numbers to letters

--- main.rb
+++ main.rb
@@ -15,37 +15,38 @@ def main_menu
   while choice != "exit"
     puts "\nWelcome to Eratos"
     puts "\nMain Menu Options:"
-    puts "1. Library Menu"
-    puts "2. Book Menu"
-    puts "3. Employee Menu"
-    puts "4. Patron Menu"
+    puts "Type L to see the Library Menu"
+    puts "Type B to see the Book Menu"
+    puts "Type E to see the Employee Menu"
+    puts "Type P to see the Patron Menu"
     puts "Exit: Exit"
     print "\nWhat is your choice? "
     choice = gets.chomp.downcase
 
-    while choice != "1" && choice != "2" && choice != "3" &&
-       choice != "4" && choice != "exit"
+    while choice != "l" && choice != "b" && choice != "e" &&
+       choice != "p" && choice != "exit"
        print "Sorry, that's an invalid choice."
        print "Please try again: "
       choice = gets.chomp.downcase
     end
 
-    if choice == "1"
+    if choice == "l"
+      puts "Heading to the Library menu"
       puts "= = = = = = = = = = = = = ="
       library_menu # method call
       puts "= = = = = = = = = = = = = ="
-    elsif choice == "2"
+    elsif choice == "b"
       puts "Heading to the Book menu"
       puts "= = = = = = = = = = = = = ="
-      puts "Which isn't real - goodbye!"
-      #book_menu
-    elsif choice == "3"
+      book_menu # method call
+      puts "= = = = = = = = = = = = = ="
+    elsif choice == "e"
       puts "Heading to the Employee menu"
       puts "= = = = = = = = = = = = = ="
       puts "Which isn't real - goodbye!"
       #employee_menu
-    elsif choice == "4"
-      puts "Heading to the researchers menu"
+    elsif choice == "p"
+      puts "Heading to the Patron menu"
       puts "= = = = = = = = = = = = = ="
       puts "Which isn't real - goodbye!"
       #patron_menu
@@ -57,40 +58,79 @@ def main_menu
 end
 
 ####################################
+####################################
 # Library Menu
 def library_menu
   choice = ""
 
-  while choice != "4"
+  while choice != "9"
     puts "\nLibrary Main Menu"
-    puts "1. List of Libraries"
-    puts "2. List of Employees"
-    puts "3. List of Books"
-    puts "4. will return you back to the main menu"
+    puts "Type L for a List of Libraries"
+    puts "Type E for a List of Employees"
+    puts "Type B for a List of Books"
+    puts "9. will return you back to the main menu"
     print "What is your choice? "
-    choice = gets.chomp
+    choice = gets.chomp.downcase
 
-    while choice != "1" && choice != "2" && choice != "3" && choice != "4"
+    while choice != "l" && choice != "e" && choice != "b" && choice != "9"
       print "Sorry, that's an invalid choice."
       print "Please try again: "
-      choice = gets.chomp
+    choice = gets.chomp.downcase
     end
 
-    if choice == "1"
+    if choice == "l"
       puts "\nHere is a list of all Libraries"
       # library_list method
-    elsif choice == "2"
+    elsif choice == "e"
       puts "\nHere is a list of all Employees"
       # employee_list method
-    elsif choice == "3"
+    elsif choice == "b"
       puts "\nHere is a list of all Books"
       # book_list method
-    elsif choice == "4"
+    elsif choice == "9"
       puts "\nSee ya!"
     end
   end
   puts "Going back to the main menu"
 end
+
+# Book Menu
+def book_menu
+  choice = ""
+
+  while choice != "9"
+    puts "\nBook Main Menu"
+    puts "Type L for a List of Libraries"
+    puts "Type E for a List of Employees"
+    puts "Type B for a List of Books"
+    puts "9. will return you back to the main menu"
+    print "What is your choice? "
+    choice = gets.chomp.downcase
+
+    while choice != "l" && choice != "e" && choice != "b" && choice != "9"
+      print "Sorry, that's an invalid choice."
+      print "Please try again: "
+    choice = gets.chomp.downcase
+    end
+
+    if choice == "l"
+      puts "\nHere is a list of all Libraries"
+      # library_list method
+    elsif choice == "e"
+      puts "\nHere is a list of all Employees"
+      # employee_list method
+    elsif choice == "b"
+      puts "\nHere is a list of all Books"
+      # book_list method
+    elsif choice == "9"
+      puts "\nSee ya!"
+    end
+  end
+  puts "Going back to the main menu"
+end
+
+####################################
+
 
 ####################################
 # this is where our driver actually starts
