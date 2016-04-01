75205a0dab1320e8fd2517fd676a067c383f8a05
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-04-01 13:29:49 -0500

    began the Library Menu

--- main.rb
+++ main.rb
@@ -7,4 +7,104 @@ require "active_record"
 require "pry"
 require "yaml"
 
+####################################
+# Library Menu
+def library_menu
+  choice = ""
+
+  while choice != "back"
+    puts "\n\n\nInsects Main Menu"
+    puts "1. List of Libraries"
+    puts "2. List of Employees"
+    puts "3. List of Books"
+    puts "Back: head back to the main menu"
+    print "What is your choice? "
+    choice = gets.chomp.downcase
+
+    while choice != "1" && choice != "2" && choice != "3" && choice != "back"
+      print "Sorry, that's an invalid choice."
+      print "Please try again: "
+      choice = gets.chomp.downcase
+    end
+
+    if choice == "1"
+      puts "Here is a list of all Libraries"
+      # library_list method
+    elsif choice == "2"
+      puts "Here is a list of all Employees"
+      # employee_list method
+    elsif choice == "3"
+      puts "Here is a list of all Books"
+      # book_list method
+    elsif choice == "back"
+      puts "See ya!"
+    end
+  end
+  puts "back to the main menu"
+end
+
+def main_menu
+  choice = ""
+
+  while choice != "exit"
+    puts "\n\nWelcome to Eratos"
+    puts "\nMain Menu Options:"
+    puts "1. Library Menu"
+    puts "2. Book Menu"
+    puts "3. Employee Menu"
+    puts "4. Paton Menu"
+    puts "Exit: Exit"
+    print "\nWhat is your choice? "
+    choice = gets.chomp.downcase
+
+    while choice != "1" && choice != "2" && choice != "3" &&
+       choice != "4" && choice != "exit"
+      print "Sorry, that's not a valid option. Try again: "
+      choice = gets.chomp.downcase
+    end
+
+    if choice == "1"
+      puts "= = = = = = = = = = = = = ="
+      library_menu # method call
+      puts "= = = = = = = = = = = = = ="
+    elsif choice == "2"
+      puts "Heading to the Book menu"
+      puts "= = = = = = = = = = = = = ="
+      puts "Which isn't real - goodbye!"
+      #book_menu
+    elsif choice == "3"
+      puts "Heading to the Employee menu"
+      puts "= = = = = = = = = = = = = ="
+      puts "Which isn't real - goodbye!"
+      #employee_menu
+    elsif choice == "4"
+      puts "Heading to the researchers menu"
+      puts "= = = = = = = = = = = = = ="
+      puts "Which isn't real - goodbye!"
+      #patron_menu
+    elsif choice == "exit"
+      puts "You chose to leave - goodbye!"
+      puts "= = = = = = = = = = = = = ="
+    end
+  end
+end
+
+# this is where our driver actually starts
+
+puts "Welcome to Our Driver Demo!"
+main_menu
+puts "Goodbye"
+
+####################################
+# Book Menu
+
+
+
+
+
+
+
+
+
+
 binding.pry
