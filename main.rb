a5f57cbcbc3b890e9b932b0920b97a654bb010d7
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-04-01 20:24:48 -0500

    added in menu loops for Employees and Patrons

--- main.rb
+++ main.rb
@@ -43,13 +43,11 @@ def main_menu
     elsif choice == "e"
       puts "Heading to the Employee menu"
       puts "= = = = = = = = = = = = = ="
-      puts "Which isn't real - goodbye!"
-      #employee_menu
+      employee_menu # method call
     elsif choice == "p"
       puts "Heading to the Patron menu"
       puts "= = = = = = = = = = = = = ="
-      puts "Which isn't real - goodbye!"
-      #patron_menu
+      patron_menu # method call
     elsif choice == "exit"
       puts "You chose to leave - goodbye!"
       puts "= = = = = = = = = = = = = ="
@@ -59,7 +57,7 @@ end
 
 ####################################
 ####################################
-# Library Menu
+# Library Menu 1.1
 def library_menu
   choice = ""
 
@@ -93,8 +91,14 @@ def library_menu
   end
   puts "Going back to the main menu"
 end
+####################################
+# -----sub menu     library_list
+def library_list
 
-# Book Menu
+end
+
+####################################
+# Book Menu 1.2
 def book_menu
   choice = ""
 
@@ -130,7 +134,80 @@ def book_menu
 end
 
 ####################################
+# Employee Menu 1.3
+def employee_menu
+  choice = ""
 
+  while choice != "9"
+    puts "\nBook Main Menu"
+    puts "Type L for a List of Libraries"
+    puts "Type E for a List of Employees"
+    puts "Type B for a List of Books"
+    puts "9. will return you back to the main menu"
+    print "What is your choice? "
+    choice = gets.chomp.downcase
+
+    while choice != "l" && choice != "e" && choice != "b" && choice != "9"
+      print "Sorry, that's an invalid choice."
+      print "Please try again: "
+    choice = gets.chomp.downcase
+    end
+
+    if choice == "l"
+      puts "\nHere is a list of all Libraries"
+      # library_list method
+    elsif choice == "e"
+      puts "\nHere is a list of all Employees"
+      # employee_list method
+    elsif choice == "b"
+      puts "\nHere is a list of all Books"
+      # book_list method
+    elsif choice == "9"
+      puts "\nSee ya!"
+    end
+  end
+  puts "Going back to the main menu"
+end
+####################################
+# Patron Menu 1.4
+def patron_menu
+  choice = ""
+
+  while choice != "9"
+    puts "\nLibrary Main Menu"
+    puts "Type L for a List of Libraries"
+    puts "Type E for a List of Employees"
+    puts "Type B for a List of Books"
+    puts "Type P for a List of Patrons"
+    puts "9. will return you back to the main menu"
+    print "What is your choice? "
+    choice = gets.chomp.downcase
+
+    while choice != "l" && choice != "e" && choice != "b" &&
+      choice != "p" && choice != "9"
+      print "Sorry, that's an invalid choice."
+      print "Please try again: "
+    choice = gets.chomp.downcase
+    end
+
+    if choice == "l"
+      puts "\nHere is a list of all Libraries"
+      # library_list method
+    elsif choice == "e"
+      puts "\nHere is a list of all Employees"
+      # employee_list method
+    elsif choice == "b"
+      puts "\nHere is a list of all Books"
+      # book_list method
+    elsif choice == "p"
+      puts "\nHere is a list of all Patrons"
+      # book_list method
+    elsif choice == "9"
+      puts "\nSee ya!"
+    end
+  end
+  puts "Going back to the main menu"
+end
 
 ####################################
 # this is where our driver actually starts
@@ -140,8 +217,6 @@ main_menu
 puts "Goodbye"
 
 ####################################
-# Book Menu
-
 
 
 binding.pry
